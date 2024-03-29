6026b122c97a3a2a98260966c630cb46575b36ac
matthieu debarge <debarge.matthieu@gmail.com>
2024-03-29 17:58:11 +0100

    ajout d'un composant pour éditer une reservation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {ReservationDetailComponent} from "./reservation-detail.component";
+import {ReservationEditComponent} from "./reservation-edit.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReservationDetailComponent
+    ReservationDetailComponent,
+    ReservationEditComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/reservation-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation-edit',
+  template: `
+  `,
+  styles: [`
+  `]
+})
+export class ReservationEditComponent {
+
+}
