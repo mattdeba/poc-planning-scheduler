598e82113a930d4d7832dcdbd5146209f581773c
matthieu debarge <debarge.matthieu@gmail.com>
2024-04-03 22:50:03 +0200

    bonne methode pour affichage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,97 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bryntumScheduler';
-  resources = [{id: 42, size: 2}, {id: 43, size: 1}, {id: 45, size: 1}, {id: 46, size: 1}, {id: 47, size: 1}]; // Ressources fixes
+  resources = [{id: 42}, {id: 43}, {id: 45}, {id: 46}, {id: 47}]; // Ressources fixes
+  displayedDates = [new Date('2024/04/01'), new Date('2024/04/02'), new Date('2024/04/03'), new Date('2024/04/04')]
   reservations = [
-    {startDate: new Date(), endDate: new Date(), resource: 42, username: 'Alice' },
-    {startDate: new Date('2024/03/27'), endDate: new Date('2024/04/01'), resource: 43, username: 'Bob'},
-    {startDate: new Date('2024/04/01'), endDate: new Date('2024/04/02'), resource: 45, username: 'Matthieu'},
-    {startDate: new Date('2024/04/10'), endDate: new Date('2024/04/12'), resource: 47, username: 'Alfred'},
+    {startDate: new Date('2024/04/01'), endDate: new Date('2024/04/02'), resource: 42, username: 'Christine'},
+    {startDate: new Date('2024/04/02'), endDate: new Date('2024/04/03'), resource: 42, username: 'Matthieu'},
   ]
-  startDateCalendar = new Date(); // Date de début, vous pouvez la définir comme vous le souhaitez
-  selectedReservation: {startDate: Date, endDate: Date, resource: number, username: string} | null;
-  modalPosition: { x: number, y: number };
-  enableScroll = true;
-  showEdition = false;
+  reservationsByDate: any = null;
 
-  showModal(reservation: {startDate: Date, endDate: Date, resource: number, username: string}, event: MouseEvent): void {
-    this.selectedReservation = reservation;
-    this.modalPosition = { x: event.clientX, y: event.clientY };
-    this.enableScroll = false;
-  }
-  closeModal(): void {
-    this.selectedReservation = null;
-    this.enableScroll = true;
-  }
-
-  getDates(): string[] {
-    let dates = [];
-    for(let i = 0; i < 7; i++) {
-      let date = new Date(this.startDateCalendar);
-      date.setDate(this.startDateCalendar.getDate() + i);
-      dates.push(date.toLocaleDateString());
-    }
-    return dates;
+  groupByDates() {
+    this.reservationsByDate = this.displayedDates.reduce<Record<string, any>>((acc, date) => {
+      let dateKey = new Date(date);
+      dateKey.setHours(0, 0, 0, 0);
+      this.resources.forEach(resource => {
+        let resourceDateKey = `${resource.id}-${dateKey.toISOString()}`;
+        acc[resourceDateKey] = this.reservations.filter(reservation => {
+          let reservationStartDate = new Date(reservation.startDate);
+          reservationStartDate.setHours(0, 0, 0, 0);
+          let reservationEndDate = new Date(reservation.endDate);
+          reservationEndDate.setHours(0, 0, 0, 0);
+          return dateKey.getTime() >= reservationStartDate.getTime() && dateKey.getTime() <= reservationEndDate.getTime() && reservation.resource === resource.id;
+        });
+      });
+      return acc;
+    }, {});
   }
 
-  getGridColumn(reservation: {startDate: Date, endDate: Date, resource: number}, resource: number): {gridSpan: string, roundedLeft: boolean, roundedRight: boolean} | null {
-    const dates = this.getDates();
-    if (reservation.resource != resource) {
-      return null;
-    }
-    let startIndex: number | null = null;
-    let endIndex: number | null = null;
-    let roundedLeft = false;
-    let roundedRight = false;
-    let gridSpan: string | null = null;
-    for (let i = 0; i < dates.length; i++) {
-      if (dates[i] === reservation.startDate.toLocaleDateString()) {
-        startIndex = i;
-      }
-      if (dates[i] === reservation.endDate.toLocaleDateString()) {
-        endIndex = i;
-      }
-    }
-    if (startIndex !== null && endIndex !== null) {
-      gridSpan = `${startIndex + 2}/${endIndex + 3}`;
-      roundedLeft = true;
-      roundedRight = true;
-    } else if (startIndex !== null && endIndex === null) {
-      gridSpan = `${startIndex + 2}/${dates.length + 3}`;
-      roundedLeft = true;
-    } else if (startIndex === null && endIndex !== null) {
-      gridSpan = `2/${endIndex + 3}`;
-      roundedRight = true;
-    } else if (startIndex === null && endIndex === null) {
-      let dateAfterSevenDays = new Date(this.startDateCalendar);
-      dateAfterSevenDays.setDate(this.startDateCalendar.getDate() + 6);
-
-      if (reservation.startDate < this.startDateCalendar && reservation.endDate > dateAfterSevenDays) {
-        gridSpan = `2/${dates.length + 3}`;
-        roundedLeft = false;
-        roundedRight = false;
-      }
-    }
-    return gridSpan ? {gridSpan, roundedLeft, roundedRight} : null;
+  getGridTemplateHeaderColumns() {
+    return '1fr ' + this.displayedDates.map(() => '1fr').join(' ');
   }
 
-  nextWeek(): void {
-    this.startDateCalendar.setDate(this.startDateCalendar.getDate() + 7);
+  getGridTemplateRows(resource: any) {
+    let maxReservations = 0;
+    this.displayedDates.forEach(date => {
+      let resourceDateKey = `${resource.id}-${date.toISOString()}`;
+      let reservationsCount = this.reservationsByDate[resourceDateKey]?.length || 0;
+      maxReservations = Math.max(maxReservations, reservationsCount);
+    });
+    return Array(maxReservations).fill('50px').join(' ');
   }
 
-  prevWeek(): void {
-    this.startDateCalendar.setDate(this.startDateCalendar.getDate() - 7);
-  }
-
-  addReservation(reservation: {startDate: Date, endDate: Date,  resource: number, username: string}): void {
-    this.reservations.push(reservation);
-  }
-
-  generateGridTemplateRows(size: number): string {
-    let rows = '';
-    for(let i = 0; i < size; i++) {
-      rows += '100px ';
-    }
-    return rows.trim();
+  ngOnInit() {
+    this.groupByDates();
   }
 }
