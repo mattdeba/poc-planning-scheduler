014db95fd2f8c7fe51e3b8678a30d5917a99ad59
matthieu debarge <debarge.matthieu@gmail.com>
2024-04-04 14:31:19 +0200

    remettre le bouton prev next

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,86 +7,78 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bryntumScheduler';
-  resources = [{id: 42}, {id: 43}, {id: 45}, {id: 46}, {id: 47}]; // Ressources fixes
-  displayedDates = [new Date('2024/04/01'), new Date('2024/04/02'), new Date('2024/04/03'), new Date('2024/04/04')]
+  resources = [{id: 42}, {id: 43}, {id: 45}, {id: 46}, {id: 47}];
+  displayedDates = [new Date('2024/04/01'), new Date('2024/04/02'), new Date('2024/04/03'), new Date('2024/04/04'),
+    new Date('2024/04/05'), new Date('2024/04/06'), new Date('2024/04/07'), new Date('2024/04/08'),
+    new Date('2024/04/09'), new Date('2024/04/10'), new Date('2024/04/11'), new Date('2024/04/12'),
+    new Date('2024/04/13'), new Date('2024/04/14'), new Date('2024/04/15')
+  ]
   reservations = [
     {id: 1, startDate: new Date('2024/04/01'), endDate: new Date('2024/04/02'), resource: 42, username: 'Christine'},
     {id: 2, startDate: new Date('2024/04/02'), endDate: new Date('2024/04/03'), resource: 42, username: 'Matthieu'},
     {id: 3, startDate: new Date('2024/04/04'), endDate: new Date('2024/04/05'), resource: 42, username: 'Paul'},
     {id: 4, startDate: new Date('2024/03/30'), endDate: new Date('2024/04/01'), resource: 42, username: 'Clémence'},
     {id: 5, startDate: new Date('2024/03/30'), endDate: new Date('2024/04/05'), resource: 42, username: 'Michel'},
-    {id: 1, startDate: new Date('2024/04/01'), endDate: new Date('2024/04/02'), resource: 43, username: 'Christine'},
-    {id: 2, startDate: new Date('2024/04/02'), endDate: new Date('2024/04/03'), resource: 43, username: 'Matthieu'},
-    {id: 3, startDate: new Date('2024/04/04'), endDate: new Date('2024/04/05'), resource: 43, username: 'Paul'},
-    {id: 4, startDate: new Date('2024/03/30'), endDate: new Date('2024/04/01'), resource: 43, username: 'Clémence'},
-    {id: 5, startDate: new Date('2024/03/30'), endDate: new Date('2024/04/05'), resource: 43, username: 'Michel'},
   ]
-  reservationsByDate: any = null;
-  reservationsByResource: any = null;
-  gridStyles: any = {};
 
-  groupByDates() {
-    this.reservationsByDate = this.displayedDates.reduce<Record<string, any>>((acc, date) => {
-      let dateKey = new Date(date);
-      dateKey.setHours(0, 0, 0, 0);
-      this.resources.forEach(resource => {
-        let resourceDateKey = `${resource.id}-${dateKey.toISOString()}`;
-        acc[resourceDateKey] = this.reservations.filter(reservation => {
-          let reservationStartDate = new Date(reservation.startDate);
-          reservationStartDate.setHours(0, 0, 0, 0);
-          let reservationEndDate = new Date(reservation.endDate);
-          reservationEndDate.setHours(0, 0, 0, 0);
-          return dateKey.getTime() >= reservationStartDate.getTime() && dateKey.getTime() <= reservationEndDate.getTime() && reservation.resource === resource.id;
-        }).sort((a, b) => {
-          let dateComparison = a.startDate.getTime() - b.startDate.getTime();
-          return dateComparison === 0 ? a.id - b.id : dateComparison;
-        });
-      });
-      return acc;
-    }, {});
+  next(): void {
+    this.displayedDates = this.displayedDates.map(date => {
+      const newDate = new Date(date);
+      newDate.setDate(newDate.getDate() + this.displayedDates.length);
+      return newDate;
+    });
   }
 
-  getGridTemplateHeaderColumnsCount() {
-    return this.displayedDates.length;
+  prev(): void {
+    this.displayedDates = this.displayedDates.map(date => {
+      const newDate = new Date(date);
+      newDate.setDate(newDate.getDate() - this.displayedDates.length);
+      return newDate;
+    });
   }
 
   getGridTemplateHeaderColumns() {
-    let count = this.getGridTemplateHeaderColumnsCount();
-    return '1fr ' + Array(count).fill('1fr').join(' ');
-  }
-
-  getNbReservations(resource: any) {
-    return this.reservations.filter(reservation => reservation.resource === resource.id).length;
-  }
-
-  getGridTemplateRows(resource: any) {
-    let maxReservations = this.getNbReservations(resource);
-    return Array(maxReservations).fill('50px').join(' ');
+    const nbDates = this.displayedDates.length;
+    return '1fr ' + Array(nbDates).fill('1fr').join(' ');
   }
 
   getGridRowStyles(resource: any) {
+    const reservations = this.getReservationsByResource(resource.id);
     return {
       'grid-template-columns': this.getGridTemplateHeaderColumns(),
-      'grid-template-rows': this.getGridTemplateRows(resource)
+      'grid-template-rows': Array(reservations.length).fill('50px').join(' ')
     };
   }
 
   getResourceNameCellStyles(resource: any) {
+    const reservations = this.getReservationsByResource(resource.id);
     return {
-      'grid-row': '1 / ' + (this.getNbReservations(resource) + 1)
+      'grid-row': '1 / ' + (reservations.length + 1)
     };
   }
 
-  groupByResources() {
-    this.reservationsByResource = this.resources.reduce<Record<number, any>>((acc, resource) => {
-      acc[resource.id] = this.reservations.filter(reservation => reservation.resource === resource.id);
-      return acc;
-    }, {});
+  getReservationsByResource(resourceId: number): any[] {
+    const displayedDatesSet = new Set(this.displayedDates.map(date => date.getTime()));
+    return this.reservations.filter(reservation => {
+      if (reservation.resource !== resourceId) {
+        return false;
+      }
+      for (let date = new Date(reservation.startDate); date <= reservation.endDate; date.setDate(date.getDate() + 1)) {
+        if (displayedDatesSet.has(date.getTime())) {
+          return true;
+        }
+      }
+      return false;
+    });
   }
+
   getReservationCoordinates(reservation: any, resourceId: number) {
-    const reservations = this.reservationsByResource[resourceId];
+    const reservations = this.getReservationsByResource(resourceId);
     const reservationIndex = reservations.findIndex((r: any) => r.id === reservation.id) + 1;
     const reservationStyle = this.getGridColumn(reservation);
+    if (!reservationStyle) {
+      return null
+    }
     return {
       'grid-row': `${reservationIndex}/${reservationIndex + 1}`,
       'grid-column': reservationStyle.gridSpan,
@@ -133,16 +125,5 @@ export class AppComponent {
       }
     }
     return gridSpan ? {gridSpan, roundedLeft, roundedRight} : null;
-  }
-
-  ngOnInit() {
-    this.groupByDates();
-    this.groupByResources();
-    this.resources.forEach(resource => {
-      this.gridStyles[resource.id] = {
-        gridTemplateColumns: this.getGridTemplateHeaderColumns(),
-        gridTemplateRows: this.getGridTemplateRows(resource)
-      };
-    });
   }
 }
